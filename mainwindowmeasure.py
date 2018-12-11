c7e3c7c123acc7c6caed6e74a58c2c4f216c4075
igrekus <upload234@mail.ru>
2018-12-11 13:54:22 +0300

    fix: don't disable measure button

--- mainwindowmeasure.py
+++ mainwindowmeasure.py
@@ -122,7 +122,7 @@ class MainWindowMeasure(QMainWindow):
     def procActMeasure(self):
         if self._domainModel.instrumentsReady:
             self._uiFacade.requestMeasure()
-            self._ui.btnMeasure.setEnabled(False)
+            # self._ui.btnMeasure.setEnabled(False)
             self._ui.btnMeasureHarmonic.setEnabled(False)
             self._ui.btnFindInstruments.setEnabled(True)
             self._ui.spinCutoffMagnitude.setEnabled(True)
