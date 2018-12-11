073f20074b6e011c95875b061e02386f46fbda1f
igrekus <upload234@mail.ru>
2018-12-11 13:00:42 +0300

    Add double-triple plot

--- plotwidget.py
+++ plotwidget.py
@@ -21,8 +21,10 @@ class PlotWidget(QObject):
         self.freqs = list()
         self.amps = list()
 
-        self.cutoff_freq_x = list()
-        self.cutoff_freq_y = list()
+        self.codes = list()
+        self.cutoff_freqs = list()
+        self.loss_double_freq = list()
+        self.loss_triple_freq = list()
 
         self.cutoff_freq_delta_y = list()
         self.cutoff_freq_delta_x = list()
@@ -42,6 +44,10 @@ class PlotWidget(QObject):
         self.harmonicFigure = None
         self.harmonicCanvas = None
         self.harmonicToolbar = None
+
+        self.doubleTripleFigure = None
+        self.doubleTripleCanvas = None
+        self.doubleTripleToolbar = None
 
     def initPlots(self):
         plt.figure(num=1)
@@ -72,52 +78,70 @@ class PlotWidget(QObject):
         self.parent()._ui.harmonicLayout.addWidget(self.harmonicCanvas)
         self.parent()._ui.harmonicLayout.addWidget(self.harmonicCanvas.toolbar)
 
+        plt.figure(num=5)
+        self.doubleTripleFigure = plt.figure(5)
+        self.doubleTripleCanvas = FigureCanvas(self.doubleTripleFigure)
+        self.doubleTripleToolbar = NavToolbar(canvas=self.doubleTripleCanvas, parent=None)
+        self.parent()._ui.layoutDoubleTriple.addWidget(self.doubleTripleCanvas)
+        self.parent()._ui.layoutDoubleTriple.addWidget(self.doubleTripleCanvas.toolbar)
+
         self.resetPlots()
 
     def resetPlots(self):
         # TODO: use self.XXXfigure
         plt.figure(1)
         plt.subplots_adjust(bottom=0.150)
-        # plt.axhline(self._cutoffMag, 0, 1, linewidth=0.8, color="0.3", linestyle="--")
-        plt.title("Коэффициент преобразования")
-        plt.xscale("log")
-        plt.xlabel("F, Гц")
-        plt.ylabel("К-т пр., дБ")
+        # plt.axhline(self._cutoffMag, 0, 1, linewidth=0.8, color='0.3', linestyle='--')
+        plt.title('Коэффициент преобразования')
+        plt.xscale('log')
+        plt.xlabel('F, Гц')
+        plt.ylabel('К-т пр., дБ')
         plt.ylim([-60, 30])
-        plt.grid(b=True, which="minor", color="0.7", linestyle='--')
-        plt.grid(b=True, which="major", color="0.5", linestyle='-')
+        plt.grid(b=True, which='minor', color='0.7', linestyle='--')
+        plt.grid(b=True, which='major', color='0.5', linestyle='-')
 
         plt.figure(2)
         plt.subplots_adjust(bottom=0.150)
-        plt.title("Частота среза по уровню " + str(self._cutoffMag) + " дБ")
-        plt.xlabel("Код")
-        plt.ylabel("F, МГц")
-        plt.yscale("log")
-        plt.grid(b=True, which="minor", color="0.7", linestyle='--')
-        plt.grid(b=True, which="major", color="0.5", linestyle='-')
+        plt.title('Частота среза по уровню ' + str(self._cutoffMag) + ' дБ')
+        plt.xlabel('Код')
+        plt.ylabel('F, МГц')
+        plt.yscale('log')
+        plt.grid(b=True, which='minor', color='0.7', linestyle='--')
+        plt.grid(b=True, which='major', color='0.5', linestyle='-')
 
         plt.figure(3)
         plt.subplots_adjust(bottom=0.150)
-        plt.title("Дельта частоты среза")
-        plt.xlabel("Код")
-        plt.ylabel("dF, МГц")
+        plt.title('Дельта частоты среза')
+        plt.xlabel('Код')
+        plt.ylabel('dF, МГц')
         plt.grid(True)
 
         plt.figure(4)
         plt.subplots_adjust(bottom=0.150)
-        plt.title("Коэффициент преобразования для N-гармоники")
-        plt.xscale("log")
-        plt.xlabel("F, Гц")
-        plt.ylabel("К-т пр., дБ")
+        plt.title('Коэффициент преобразования для N-гармоники')
+        plt.xscale('log')
+        plt.xlabel('F, Гц')
+        plt.ylabel('К-т пр., дБ')
         plt.ylim([-60, 30])
-        plt.grid(b=True, which="minor", color="0.7", linestyle='--')
-        plt.grid(b=True, which="major", color="0.5", linestyle='-')
+        plt.grid(b=True, which='minor', color='0.7', linestyle='--')
+        plt.grid(b=True, which='major', color='0.5', linestyle='-')
+
+        plt.figure(5)
+        plt.subplots_adjust(bottom=0.150)
+        plt.title('Затухание на двойной и тройной частоте среза')
+        plt.xscale('log')
+        plt.xlabel('Код')
+        plt.ylabel('Подавление, дБ')
+        plt.ylim([-60, 30])
+        plt.grid(b=True, which='minor', color='0.7', linestyle='--')
+        plt.grid(b=True, which='major', color='0.5', linestyle='-')
 
     def clearFigures(self):
         plt.figure(1).clear()
         plt.figure(2).clear()
         plt.figure(3).clear()
         plt.figure(4).clear()
+        plt.figure(5).clear()
         self.resetPlots()
 
     def parseFreqStr(self, string):
@@ -128,7 +152,7 @@ class PlotWidget(QObject):
 
     def exportPics(self, fmt='png'):
         if fmt.lower() == 'png':
-            img_path = ".\\image\\"
+            img_path = '.\\image\\'
             try:
                 os.makedirs(img_path)
             except OSError as ex:
@@ -165,7 +189,7 @@ class PlotWidget(QObject):
         #     plt.yticks(list(range(-60, 30, 10)) + [self._cutoffMag])
         #     self._cutoffTickAdded = True
 
-        plt.plot(freq, amp, color="0.4")
+        plt.plot(freq, amp, color='0.4')
 
         self.mainFigure.canvas.draw()
 
@@ -177,29 +201,46 @@ class PlotWidget(QObject):
         cutoff_mag = max_amp + self._cutoffMag
 
         for a, f in zip(self.amps, self.freqs):
-            self.cutoff_freq_y.append(f[a.index(min(a, key=lambda x: abs(cutoff_mag - x)))])
+            cutoff_freq = f[a.index(min(a, key=lambda x: abs(cutoff_mag - x)))]
+            self.cutoff_freqs.append(cutoff_freq)
 
-        self.cutoff_freq_y = list(reversed(self.cutoff_freq_y))
-        self.cutoff_freq_x = range(len(self.cutoff_freq_y))
+            double_f = cutoff_freq * 2
+            triple_f = cutoff_freq * 3
+            double_f_index = f.index(min(f, key=lambda x: abs(double_f - x)))
+            triple_f_index = f.index(min(f, key=lambda x: abs(triple_f - x)))
+
+            self.loss_double_freq.append(a[double_f_index])
+            self.loss_triple_freq.append(a[triple_f_index])
+
+        self.cutoff_freqs = list(reversed(self.cutoff_freqs))
+        self.codes = range(len(self.cutoff_freqs))
 
         fig = plt.figure(1)
-        plt.axhline(cutoff_mag, 0, 1, linewidth=0.8, color="0.3", linestyle="--")
+        plt.axhline(cutoff_mag, 0, 1, linewidth=0.8, color='0.3', linestyle='--')
         plt.yticks(list(plt.yticks()[0]) + [cutoff_mag])
         fig.canvas.draw()
 
         fig = plt.figure(2)
-        plt.plot(self.cutoff_freq_x, self.cutoff_freq_y, color="0.4")
+        plt.plot(self.codes, self.cutoff_freqs, color='0.4')
         fig.canvas.draw()
 
         # for i in range(len(self.cutoff_freq_y[:-2])):
-        for i in range(len(self.cutoff_freq_y[:-1])):
-            d = abs(self.cutoff_freq_y[i + 1] - self.cutoff_freq_y[i])
+        for i in range(len(self.cutoff_freqs[:-1])):
+            d = abs(self.cutoff_freqs[i + 1] - self.cutoff_freqs[i])
             self.cutoff_freq_delta_y.append(d)
 
         self.cutoff_freq_delta_x = range(len(self.cutoff_freq_delta_y))
 
         fig = plt.figure(3)
-        plt.plot(self.cutoff_freq_delta_x, self.cutoff_freq_delta_y, color="0.4")
+        plt.plot(self.cutoff_freq_delta_x, self.cutoff_freq_delta_y, color='0.4')
+        fig.canvas.draw()
+
+        fig = plt.figure(5)
+        try:
+            plt.plot(self.codes, self.loss_double_freq, color='blue')
+            plt.plot(self.codes, self.loss_triple_freq, color='red')
+        except Exception as ex:
+            print(ex)
         fig.canvas.draw()
 
     @pyqtSlot(name='harmonicMeasured')
@@ -214,6 +255,6 @@ class PlotWidget(QObject):
 
         fig = plt.figure(4)
         plt.plot(xs, ys, color='0.4')
-        plt.axhline(cutoff_mag, 0, 1, linewidth=0.8, color="0.3", linestyle="--")
+        plt.axhline(cutoff_mag, 0, 1, linewidth=0.8, color='0.3', linestyle='--')
         plt.yticks(list(plt.yticks()[0]) + [cutoff_mag])
         fig.canvas.draw()
